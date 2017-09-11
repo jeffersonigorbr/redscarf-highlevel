63d2e1869f9f1806cfcc7c7ebc0290bd25de5106
Jefferson Silva <jefferson.duarte@ifrn.edu.br>
2017-09-11 13:15:09 -0300

    buffer versão 0.4 alpha

--- buffer.cpp
+++ buffer.cpp
@@ -1,26 +1,17 @@
 #include "buffer.h"
 
-<<<<<<< HEAD
 buffer::buffer(sc_int capacidade){
-=======
-Buffer::Buffer(int capacidade){
->>>>>>> 64a29e497c6e27993ac63f8e36b30774b0e199e6
 	this->length = capacidade;
 }
 
-Buffer::estaVazio(){
-	if (this->posicoes.size() == 0){
+buffer::estaVazio(){
+	if (this->posicoes.empty()){
 		return 1;
 	} else {
 		return 0;
 	}
 }
-
-<<<<<<< HEAD
 buffer::add(sc_int valor){
-=======
-Buffer::add(int valor){
->>>>>>> 64a29e497c6e27993ac63f8e36b30774b0e199e6
 	if (this->posicoes >= this->length){
 		wait(read_event);
 		return -1; // pilha virtualmente cheia
@@ -28,18 +19,15 @@ Buffer::add(int valor){
 		this->posicoes.push(valor);
 		write_event.notify();
 	}
-
-Buffer::remove(){
+buffer::remove(){
 	if (this->posicoes < 1){
 		wait(write_event);
 		return 0; // erro devido a fila vazia
 	} else {
 		read_event.notify();
-		return (this->posicoes.pop());
+		return this->posicoes.pop();
 	}
-<<<<<<< HEAD
 }	
-
 buffer::estaCheio(){
 	if (this->posicoes.size == this->length){
 		return 1;
@@ -47,12 +35,3 @@ buffer::estaCheio(){
 		return 0;
 		}
 }
-	
-		
-	
-=======
-}
-
-
-
->>>>>>> 64a29e497c6e27993ac63f8e36b30774b0e199e6
--- buffer.h
+++ buffer.h
@@ -2,11 +2,7 @@
 #include "systemc.h"
 #include "interfaces.cpp"
 
-<<<<<<< HEAD
 class buffer : public sc_channel, public write_if, public read_if {
-=======
-class Buffer {
->>>>>>> 64a29e497c6e27993ac63f8e36b30774b0e199e6
         private:
         sc_int<32> length;
         queue <sc_int<32>> posicoes;
@@ -14,17 +10,8 @@ class Buffer {
         sc_logic estaVazio(){}
         sc_event write_event, read_event;
         public:
-<<<<<<< HEAD
         void add(sc_int){}
         sc_int remove(){}
         buffer(sc_int){}
                         
 }                
-=======
-        bool estaVazio(){}
-        void add(int){}
-        bool remove(){}
-        buffer(int){}
-
-}
->>>>>>> 64a29e497c6e27993ac63f8e36b30774b0e199e6
