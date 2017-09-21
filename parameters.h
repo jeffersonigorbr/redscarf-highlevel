b3100263ba39930abd01c83587b47b4274fd4a66
orionsamuel <s.oliveira.15@hotmail.com>
2017-09-20 23:35:06 -0300

    Roteamento parcialmente feito

---
+++ parameters.h
@@ -0,0 +1,17 @@
+#ifndef PARAMETERS_H_INCLUDED
+#define PARAMETERS_H_INCLUDED
+
+#define NORTE 0
+#define SUL 1
+#define LESTE 2
+#define OESTE 3
+#define CHEGADA 4
+
+struct Coordenada{
+
+int x;
+int y;
+
+};
+
+#endif 
--- routing.h
+++ routing.h
@@ -1,11 +1,15 @@
 #include "systemc.h"
+#include "parameters.h"
 
 using namespace std;
 
-class Routing
-{
+class Routing{
+
+	Coordenada destino;
+	Coordenada posicao;
 
 public:
     Routing();
     ~Routing();
+    routing_xy(Coordenada posicao, Coordenada destino);
 }
