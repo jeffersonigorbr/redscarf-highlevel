d95326f42fa01a398e636efeafc171400ff117e8
Adelino Afonso <aafavelino@icloud.com>
2017-10-04 00:34:10 -0300

    rede 2x2

--- Rede_SOCin/src/main.cpp
+++ Rede_SOCin/src/main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	std::vector<std::vector<Roteador> >  rede(2, vector<Roteador>(2)); // Matriz A
-	
+	//Criando uma rede 2x2
 	Roteador *roteador1 = new Roteador();
 	Roteador *roteador2 = new Roteador();
 	Roteador *roteador3 = new Roteador();
